d19dc7d73d100876b8c06defec4ee11430990a5d
Svyatoslav29 <svyatoslav29@gmail.com>
2021-10-17 19:48:33 +0300

    test hw021

---
+++ src/components/Feedback/feedback.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './feedback.module.css';
+
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+      <ul className={style.item}>
+        {options.map(el => (
+          <li key={el}>
+            <button type="button" name={el} onClick={onLeaveFeedback}  className={style.list}>
+              {el}
+            </button>
+          </li>
+        ))}
+      </ul>
+  );
+}
+
